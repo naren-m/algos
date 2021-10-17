09f9a081a49cf01449e6a51dc697469bb17990d9
Naren Mudivarthy <narenuday595@gmail.com>
2021-10-16 18:40:15 -0700

    Linked list problems

--- problems/linked_list.py
+++ problems/linked_list.py
@@ -1,9 +1,41 @@
 import context
 
+import unittest
+
 from data_structures.linked_list import LinkedList
 
-class Problems:
+def _createLL(n=9):
+    ll = LinkedList()
 
+    for i in range(0, n):
+        ll.append(i)
+
+    return ll
+
+def _createFromList(l):
+    ll = LinkedList()
+
+    for i in l:
+        ll.append(i)
+
+    return ll
+
+def _convertArr(l):
+    # Create an array of the
+    # required length
+    arr = []
+    curr = l.head
+ 
+    # Traverse the Linked List and add the
+    # elements to the array one by one
+    while (curr != None):
+        arr.append(curr.data)
+        curr = curr.next
+     
+    # Print the created array
+    return arr
+
+class Problems:
     def find_mid(self, ll):
         fast = ll.head
         slow = ll.head
@@ -14,20 +46,37 @@ class Problems:
 
         return slow
 
-def _createLL(n=9):
-    ll = LinkedList()
+    def lc_add_two_numbers(self, l1, l2):
+        max_len = max(l1.size, l2.size)
+        l1c = l1.head
+        l2c = l2.head
+        o = LinkedList()
+        carry = 0
+        for i in range(0, max_len):
 
-    for i in range(0, n):
-        ll.append(i)
+            s = l1c.data + l2c.data + carry
+            carry = s % 10
+            s = s // 10
+            o.append(s)
+            print(o.head)
 
-    return ll
+        
+        return o
 
 
-def main():
-    p = Problems()
-    ll = _createLL()
-    mid = p.find_mid(ll)
-    print(mid.data)
+class TestProblems(unittest.TestCase):
+    def test_find_min(self):
+        p = Problems()
+        ll = _createLL(n=9)
+        mid = p.find_mid(ll)
+        print(mid.data)
+        self.assertEqual(mid.data, 4)
 
-if __name__ == "__main__":
-    main()
+    def test_lc_add_two_numbers(self):
+        p = Problems()
+        l1 = _createFromList([2,4,3])
+        l2 = _createFromList([5,6,4])
+        print(l1.head, l2.head)
+        o = p.lc_add_two_numbers(l1, l2)
+        o = _convertArr(o)
+        self.assertEqual(o, [7,0,8])
